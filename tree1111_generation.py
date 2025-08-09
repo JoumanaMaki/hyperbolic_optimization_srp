9e546d8b6f7ff386a6362b1670dd322ed5f2e1a8
Elvin Guseinov <elwinguseinov@gmail.com>
2025-08-09 17:17:13 +0200

    feat: Added script for generating tree1111 and generated 9 datasets

---
+++ tree1111_generation.py
@@ -0,0 +1,45 @@
+import numpy as np
+import scipy.sparse as sp
+import os
+import pandas as pd
+
+gammas = [0.0, 0.05, 0.10, 0.15, 0.2, 0.4, 0.6, 0.8, 1.0]
+
+def generate_tree1111(gamma):
+    feats_tree1111 = np.zeros((1111, 1000))
+    labels_tree1111 = np.zeros(1111)
+
+    # Root
+    feats_tree1111[0] = np.random.randn(1000)
+
+    # Level 1
+    for i in range(1, 11):
+        feats_tree1111[i] = gamma * feats_tree1111[0] + np.random.randn(1000)
+
+    # Level 2
+    for i in range(1, 11):
+        for j in range(i * 10 + 1, i * 10 + 11):
+            feats_tree1111[j] = gamma * feats_tree1111[i] + np.random.randn(1000)
+
+    # Level 3
+    for i in range(11, 111):
+        for j in range(111 + (i - 11) * 10, 111 + (i - 11) * 10 + 10):
+            feats_tree1111[j] = gamma * feats_tree1111[i] + np.random.randn(1000)
+
+    # Edges
+    parent_list = [(i - 1) // 10 for i in range(1, 1111)]
+    child_list = list(range(1, 1111))
+    edges_tree1111 = pd.DataFrame({'parent': parent_list, 'child': child_list})
+
+    os.makedirs('./datasets/tree1111', exist_ok=True)
+    subdir = str(gamma).replace('.', '')
+    os.makedirs(f'./datasets/tree1111/g{subdir}_lp', exist_ok=True)
+
+    edges_tree1111.to_csv(f'./datasets/tree1111/g{subdir}_lp/g{subdir}_lp.edges.csv', index=False)
+    np.save(f'./datasets/tree1111/g{subdir}_lp/g{subdir}_lp.labels.npy', labels_tree1111)
+    sp.save_npz(f'./datasets/tree1111/g{subdir}_lp/g{subdir}_lp.feats.npz', sp.csr_matrix(feats_tree1111))
+
+for gamma in gammas:
+    print(f'Generating tree1111 for gamma={gamma}')
+    generate_tree1111(gamma)
+    print(f'Finished generating tree1111 for gamma={gamma}\n')
