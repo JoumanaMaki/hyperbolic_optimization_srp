ee942855ee3d954ca1893ef803dbebd622b0e1f2
LawrenceGrigoryan <glavrentiy123@gmail.com>
2025-08-09 18:54:29 +0200

    adjust gcn to match the paper

---
+++ models/node_clf/gcn/gcn_2layer.py
@@ -0,0 +1,24 @@
+"""
+Simple GCN Model Architecture
+https://arxiv.org/pdf/1609.02907
+"""
+import torch
+from torch.nn import Linear, ReLU
+from torch_geometric.nn import GCNConv
+
+
+class GCN(torch.nn.Module):
+    def __init__(self, num_node_features: int, hidden_channels: int, num_classes: int):
+        super(GCN, self).__init__()
+        self.conv1 = GCNConv(num_node_features, hidden_channels)
+        self.relu = ReLU()
+        self.conv2 = GCNConv(hidden_channels, hidden_channels)
+        self.out = Linear(hidden_channels, num_classes)
+
+    def forward(self, x: torch.tensor) -> torch.tensor:
+        x = self.conv1(x)
+        x = self.relu(x)
+        x = self.conv2(x)
+        x = self.out(x)
+        
+        return x
