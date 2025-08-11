4328a37d41f7e7b075134922ede82a35c54a88f1
LawrenceGrigoryan <glavrentiy123@gmail.com>
2025-08-11 22:52:57 +0200

    minor adjustment

--- models/node_clf/gcn/gcn.py
+++ models/node_clf/gcn/gcn.py
@@ -9,7 +9,7 @@ from torch_geometric.nn import GCNConv
 
 class GCN(torch.nn.Module):
     def __init__(self, num_node_features: int, hidden_channels: int, num_classes: int):
-        super(GCN, self).__init__()
+        super().__init__()
         self.conv1 = GCNConv(num_node_features, hidden_channels)
         self.relu = ReLU()
         self.conv2 = GCNConv(hidden_channels, hidden_channels)
