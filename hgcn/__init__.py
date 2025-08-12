8f592910c8d803e0d85206e1ad709df1addb7b04
LawrenceGrigoryan <glavrentiy123@gmail.com>
2025-08-12 18:45:46 +0200

    add pre-defined hgcn models

---
+++ hgcn/__init__.py
@@ -0,0 +1,5 @@
+"""
+Code from official repository for the paper `Hyperbolic Graph Convolutional Neural Networks`
+https://arxiv.org/pdf/1910.12933
+https://github.com/HazyResearch/hgcn/tree/master
+"""
