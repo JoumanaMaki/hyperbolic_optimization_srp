f4230a17300df0320113d198584b0e1969f856c0
LawrenceGrigoryan <glavrentiy123@gmail.com>
2025-08-17 18:34:05 +0200

    remove hgcn and add it as a submodule

--- hgcn/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-Code from official repository for the paper `Hyperbolic Graph Convolutional Neural Networks`
-https://arxiv.org/pdf/1910.12933
-https://github.com/HazyResearch/hgcn/tree/master
-"""
